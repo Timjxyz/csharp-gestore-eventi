033be9445448cde277141d320471d54f9e6da32d
Timothy Gaither <timjgaither@gmail.com>
2022-07-08 17:58:33 +0200

    seconda milestone

--- Evento.cs
+++ Evento.cs
@@ -8,27 +8,34 @@ namespace csharp_gestore_eventi
 {
     public class Evento
     {
-        private string titolo {
+        public string titolo;
+        public DateTime data;
+        public int capienzaMassima;
+        public int postiPrenotati { get; private set; }
+        public string Titolo {
             get{
 
-                return this.titolo;
+                return titolo;
             }
-            set { 
-                if(value == null){
-                    throw new ArgumentException( "Il nome non può essere vuoto!!");
+           set {
+                try{
+                    titolo = value;
+                    if (titolo == null)
+                        throw new Exception();
+                }catch(Exception e){
+                    Console.WriteLine("Il nome non può essere vuoto");
                 }
-                this.titolo = value;
-            }
+            } 
         }
-        private DateTime data
+        public DateTime Data
         {
             get{
-                return this.data;
+                return data;
             }
             set{
                 try
                 {
-                    this.data = value;
+                   data = value;
 
                 }
                 catch (Exception e)
@@ -38,11 +45,11 @@ namespace csharp_gestore_eventi
             }
         }
         
-        private int capienzaMassima
+        public int CapienzaMassima
         {
             get
             {
-                return this.capienzaMassima;
+                return capienzaMassima;
             }
             set
             {
@@ -52,16 +59,16 @@ namespace csharp_gestore_eventi
                         "La capienza massima non può essere inferiore o uguale a 0"
                      );
                 }
-                this.capienzaMassima = value;
+                capienzaMassima = value;
             }
         }
-        public int postiPrenotati { get; private set; }
+    
 
         public Evento(string titolo, DateTime data, int capienzaMassima)
         {
-            this.titolo = titolo;
-            this.data = data;
-            this.capienzaMassima = capienzaMassima;
+            this.Titolo = titolo;
+            this.Data = data;
+            this.CapienzaMassima = capienzaMassima;
             this.postiPrenotati = 0;
         }
         //. PrenotaPosti: aggiunge i posti passati come parametro ai posti prenotati. Se
@@ -71,7 +78,7 @@ namespace csharp_gestore_eventi
         {
             try
             {
-                if (DateTime.Now.CompareTo(this.data) > 0 || this.capienzaMassima == 0 ||  this.capienzaMassima - this.postiPrenotati <= 0)
+                if (DateTime.Now.CompareTo(this.Data) > 0 || this.CapienzaMassima == 0 ||  this.CapienzaMassima - this.postiPrenotati <= 0)
                 {
                     throw new Exception();
                 }
@@ -84,15 +91,19 @@ namespace csharp_gestore_eventi
         //DisdiciPosti: riduce del i posti prenotati del numero di posti indicati come
         //parametro.Se l’evento è già passato o non ci sono i posti da disdire
         //sufficienti, deve sollevare un’eccezione
-        public void DisdiciPostii(int disdiciPosti)
+        public int DisdiciPosti(int disdiciPosti)
         {
             try
             {
-                if (DateTime.Now.CompareTo(this.data) > 0 || this.postiPrenotati < disdiciPosti)
+                if (DateTime.Now.CompareTo(this.Data) > 0 || this.postiPrenotati < disdiciPosti)
                 {
                     throw new Exception();
                 }
                 this.postiPrenotati -= disdiciPosti;
+                Console.WriteLine("");
+                Console.WriteLine("Operazione completata con successo");
+                Console.WriteLine("");
+                return this.postiPrenotati;
 
             }
             catch (Exception e)
@@ -100,6 +111,7 @@ namespace csharp_gestore_eventi
                 Console.WriteLine("******************");
                 Console.WriteLine("Non è possibile disdire il posto prenotato,assicurati di aver inserito i dati correttamente");
                 Console.WriteLine("******************");
+                return 0;
             }
         }
 
@@ -110,8 +122,171 @@ namespace csharp_gestore_eventi
         //DateTime.
         public override string ToString()
         {
-            return this.data.ToString($"dd/MM/yyyy - {this.titolo}");
+            return this.Data.ToString($"dd/MM/yyyy - {this.Titolo}");
         }
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace csharp_gestore_eventi
+//{
+//    public class Evento
+//    {
+//        public string titolo;
+//        public DateTime data;
+//        public int capienzaMassima;
+//        public int postiPrenotati { get; private set; }
+
+//        public string Titolo
+//        {
+//            get { return titolo; }
+//            set
+//            {
+//                try
+//                {
+//                    titolo = value;
+//                    if (titolo == null)
+//                        throw new Exception();
+//                }
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine("è obbligatorio inserire il titolo!");
+//                }
+//            }
+//        }
+
+//        public DateTime Data
+//        {
+//            get { return data; }
+//            set
+//            {
+//                try
+//                {
+//                    data = value;
+
+//                    if (DateTime.Now.CompareTo(this.data) > 0)
+//                        throw new Exception();
+//                }
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine("Non puoi inserire una data già trascorsa!");
+//                }
+//            }
+//        }
+//        public int CapienzaMassima
+//        {
+//            get { return capienzaMassima; }
+//            private set
+//            {
+//                try
+//                {
+//                    capienzaMassima = value;
+
+//                    if (capienzaMassima <= 0)
+//                        throw new Exception();
+//                }
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine("La capienza deve essere un numero positivo!");
+//                }
+//            }
+//        }
+
+
+//        public Evento(string titolo, DateTime data, int capienzaMassima)
+//        {
+//            Titolo = titolo;
+//            Data = data;
+//            CapienzaMassima = capienzaMassima;
+//            this.postiPrenotati = 0;
+//        }
+
+//        public void PrenotaPosti(int numeroPosti)
+//        {
+
+//            try
+//            {
+//                if (this.capienzaMassima == 0 || DateTime.Now.CompareTo(this.data) > 0 || this.capienzaMassima - this.postiPrenotati <= 0 || this.CapienzaMassima < numeroPosti)
+//                {
+//                    throw new Exception();
+//                }
+//                this.postiPrenotati += numeroPosti;
+//                Console.WriteLine("Posti prenotati, hai riservato " + numeroPosti + " posti per te");
+
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("Errore nella prenotazione! Assicurati di aver scritto i dati correttamente");
+//            }
+//        }
+
+//        public int DisdiciPosti(int postiDaRimuovere)
+//        {
+//            try
+//            {
+//                if (DateTime.Now.CompareTo(this.data) > 0 || this.postiPrenotati < postiDaRimuovere)
+//                {
+//                    throw new Exception();
+//                }
+//                this.postiPrenotati -= postiDaRimuovere;
+//                Console.WriteLine("Disdetta avvenuta con successo!");
+
+//                return this.postiPrenotati;
+
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("Errore nella disdetta! Assicurati di aver scritto i dati correttamente");
+//                return 0;
+//            }
+
+//        }
+
+//        public override string ToString()
+//        {
+//            return this.data.ToString("dd/MM/yyyy") + "- " + this.titolo;
+//        }
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,54 @@
 
 //1.Nel vostro programma principale Program.cs, chiedete all’utente di inserire un
 //nuovo evento con tutti i parametri richiesti dal costruttore.
+
+Console.Write("Inserisci il nome dell'evento: ");
+string titolo = Console.ReadLine();
+Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+DateTime data = DateTime.Parse(Console.ReadLine());
+Console.Write("Inserisci il numero di posti totali: ");
+int capienza = int.Parse(Console.ReadLine());
+
+Evento e = new Evento(titolo, data, capienza);
+
+Console.Write("Quanti posti desideri prenotare? ");
+int postiPrenotati = int.Parse(Console.ReadLine());
+
+e.PrenotaPosti(postiPrenotati);
+Console.WriteLine("");
+Console.WriteLine($"Numero di posti prenotati = {postiPrenotati}");
+Console.WriteLine($"Numero di posti disponibili = {e.CapienzaMassima - postiPrenotati}");
+Console.WriteLine("");
+
+string sceltaUtente = null;
+while (sceltaUtente == "si" || sceltaUtente != "no")
+{
+    Console.WriteLine("");
+    Console.Write("Vuoi disdire dei posti? (si/no)? ");
+    sceltaUtente = Console.ReadLine();
+   
+    if (sceltaUtente == "si")
+    {
+        Console.Write("Indica il numero di posti da disdire: ");
+        int postiDaRimuovere = int.Parse(Console.ReadLine());
+
+        int postiPrenotatiAggionrati = e.DisdiciPosti(postiDaRimuovere);
+        int postiDisponibiliAggiornati = e.CapienzaMassima - postiPrenotatiAggionrati;
+        Console.WriteLine("");
+
+        Console.WriteLine($"Numero di posti prenotati= {postiPrenotatiAggionrati}");
+        Console.WriteLine($"Numero di posti disponibili= {postiDisponibiliAggiornati}");
+    }
+    else if (sceltaUtente == "no")
+    {
+        Console.WriteLine("Ok va bene!");
+    }
+}
+
+
+
+
+
+
+
+
